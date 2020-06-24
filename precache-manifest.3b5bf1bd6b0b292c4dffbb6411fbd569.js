c8a8f7f0f4dd4a2ccaa87c571786a8241c023ed9
lorem-ipsum <lorem-ipsum@users.noreply.github.com>
2020-06-24 07:37:19 +0000

    Automated deployment: Wed Jun 24 07:37:19 UTC 2020 ea972fa1e6302d04a406e1b77b36a8554ee4d997

---
+++ precache-manifest.3b5bf1bd6b0b292c4dffbb6411fbd569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6203c8e457490439172639bac5cc1587",
+    "url": "/chat-ui/index.html"
+  },
+  {
+    "revision": "baabc839a007f43b7411",
+    "url": "/chat-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90aedc488336d5a8edbc",
+    "url": "/chat-ui/static/js/2.6211f6ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-ui/static/js/2.6211f6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baabc839a007f43b7411",
+    "url": "/chat-ui/static/js/main.7170b944.chunk.js"
+  },
+  {
+    "revision": "f663e8a71c90927846e7",
+    "url": "/chat-ui/static/js/runtime-main.36d5b265.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
