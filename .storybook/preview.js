158b2df8f56ffba7326d9c0fad967a69a5c76e58
CLT <1330525233@qq.com>
2020-06-24 22:39:03 +0800

    add component Icon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import {addDecorator} from '@storybook/react'
-import {ThemeProvider} from 'styled-components'
+import { addDecorator, addParameters } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme'
 
-addDecorator(storyFn=>(
+addDecorator(storyFn => (
   <ThemeProvider theme={theme}>
     {storyFn()}
   </ThemeProvider>
 ))
+
+addParameters({
+  options: {
+    showRoots: true
+  }
+})
--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,10 +1,15 @@
 
 import React from 'react'
 import Icon from '.'
+import {ReactComponent as SmileIcon} from 'assets/icon/smile.svg'
 
 export default {
-  title: "Icon",
+  title: "UI components/Icon",
   component: Icon
 }
 
-export const Default = () => <Icon>DEFAULT</Icon>
+export const Default = () => <Icon icon={SmileIcon} />
+
+export const CustomColor = () => <Icon icon={SmileIcon} color="#cccccc" />
+
+export const CustomSize = () => <Icon width={48} height={48} icon={SmileIcon} color="#cccccc" />
